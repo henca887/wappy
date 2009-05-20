934e6e9c84548c79438b1b58f4902e84657e14be
unknown <Randj@.(none)>
2009-05-20 15:53:53 +0200

    Lit vy uppdatering

--- frontend/src/wappy/client/Frontend.java
+++ frontend/src/wappy/client/Frontend.java
@@ -1,132 +1,209 @@
 package wappy.client;
 
 import wappy.client.calendar.Calendar;
+import wappy.client.groups.Groups;
 import wappy.client.mail.MailClient;
 
 import com.extjs.gxt.desktop.client.Desktop;
 import com.extjs.gxt.desktop.client.Shortcut;
+import com.extjs.gxt.desktop.client.StartMenu;
+import com.extjs.gxt.desktop.client.TaskBar;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
 import com.extjs.gxt.ui.client.event.ComponentEvent;
 import com.extjs.gxt.ui.client.event.MenuEvent;
 import com.extjs.gxt.ui.client.event.SelectionListener;
+import com.extjs.gxt.ui.client.widget.VerticalPanel;
 import com.extjs.gxt.ui.client.widget.Window;
 import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.menu.MenuItem;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class Frontend implements EntryPoint {
-    Window mailWindow = new Window();
-    Window calendarWindow = new Window();
-    HorizontalPanel toolBar = new HorizontalPanel();
+	private Window mailWindow = new Window();
+    private Window calendarWindow = new Window();
+    private HorizontalPanel toolBar = new HorizontalPanel();
     
     private Calendar calendar = new Calendar();
-    private Desktop desktop = new Desktop();
+    private Desktop desktop;
     
     public void onModuleLoad() {
         mailWindow.setSize(800, 600);  
         mailWindow.setPlain(true);  
         mailWindow.setHeading("Mail");  
-        mailWindow.setLayout(new FitLayout());  
+        mailWindow.setLayout(new FitLayout());
+        mailWindow.setMinimizable(true);
+        mailWindow.setMaximizable(true);
         mailWindow.add(new MailClient());
 
         calendarWindow.setSize(800, 600);  
-        if (calendar.isGridView()) { // could use setMonitorWindowResize
-        	calendarWindow.setResizable(false);
-        }
+//        if (calendar.isGridView()) { // could use setMonitorWindowResize
+//        	calendarWindow.setResizable(false);
+//        }
         calendarWindow.setPlain(true);  
         calendarWindow.setHeading("Calendar");  
         calendarWindow.setLayout(new FitLayout());  
         calendarWindow.setMinimizable(true);
-//        calendarWindow.addWindowListener(new WindowListener(WindowEvent) {
+        calendarWindow.setMaximizable(true);
+//        calendarWindow.addWindowListener(new WindowListener() {
+//        	@Override
+//        	public void windowMinimize(WindowEvent we) {
+//        		calendarWindow.hide();
+//        		desktop.getTaskBar().addTaskButton(calendarWindow);
 //        	
+//        	}
+//        	
+//        	@Override
+//        	public void windowRestore(WindowEvent we) {
+//        		calendarWindow.show();
+//        	}
 //        });
         calendarWindow.add(calendar);
-        
-//        Button mailButton = new Button("Mail");  
-//        mailButton.addSelectionListener(new SelectionListener<ButtonEvent>() {  
-//            @Override  
-//            public void componentSelected(ButtonEvent ce) {  
-//                openMail();
-//            }  
-//        });
-//
-//        Button calendarButton = new Button("Calendar");
-//        calendarButton.addSelectionListener(new SelectionListener<ButtonEvent>() {  
-//            @Override  
-//            public void componentSelected(ButtonEvent ce) {  
-//            	openCalendar();
-//            }  
-//        });
-//        
-//        Button logoutButton = new Button("Logout");  
-//        logoutButton.addSelectionListener(new SelectionListener<ButtonEvent>() {  
-//            @Override  
-//            public void componentSelected(ButtonEvent ce) {  
-//                com.google.gwt.user.client.Window.Location.assign(
-//                    "/accounts/logout/");
-//            }  
-//        });  
-//
-//        toolBar.add(mailButton);
-//        toolBar.add(calendarButton);
-//        toolBar.add(logoutButton);
-        initDesktop();
-//        RootPanel.get().add(toolBar);
+
+//        initDesktopView();
+        initSimpleView();
+
     }
 
-	private void initDesktop() {
-		MenuItem openMail = new MenuItem("Mail",
-				new SelectionListener<MenuEvent>() {
-					@Override
-					public void componentSelected(MenuEvent ce) {
-						openMail();
-					}
-			});
-		MenuItem openCalendar = new MenuItem("Calendar",
-				new SelectionListener<MenuEvent>() {
-					@Override
-					public void componentSelected(MenuEvent ce) {
-						openCalendar();
-					}
-				});
-		MenuItem logout = new MenuItem("Logout",
-				new SelectionListener<MenuEvent>() {
-					@Override
-					public void componentSelected(MenuEvent ce) {
-						com.google.gwt.user.client.Window.Location.assign(
-	                    "/accounts/logout/");
-					}
-				});
-		openMail.setTitle("Open the Mail Client");
-		openCalendar.setTitle("Open the Calendar");
+	private void initSimpleView() {
+      Button mailButton = new Button("Mail");  
+      mailButton.addSelectionListener(new SelectionListener<ButtonEvent>() {  
+          @Override  
+          public void componentSelected(ButtonEvent ce) {  
+              openMail();
+          }  
+      });
+
+      Button calendarButton = new Button("Calendar");
+      calendarButton.addSelectionListener(new SelectionListener<ButtonEvent>() {  
+          @Override  
+          public void componentSelected(ButtonEvent ce) {  
+          	openCalendar();
+          }  
+      });
+      
+      Button logoutButton = new Button("Logout");  
+      logoutButton.addSelectionListener(new SelectionListener<ButtonEvent>() {  
+          @Override  
+          public void componentSelected(ButtonEvent ce) {  
+              com.google.gwt.user.client.Window.Location.assign(
+                  "/accounts/logout/");
+          }  
+      });  
+
+      toolBar.add(mailButton);
+      toolBar.add(calendarButton);
+      toolBar.add(logoutButton);
+      
+      VerticalPanel vp = new VerticalPanel();
+      vp.add(toolBar);
+      vp.add(new Groups());
+      
+      RootPanel.get().add(vp);
+	}
+/** 
+ * initDesktopView - Under development
+ */
+	private void initDesktopView() {
+		desktop = new Desktop();
+		desktop.getDesktop().setStyleName("wappy-desktop-background");
+		final Shortcut mailShortcut = new Shortcut();
+		final Shortcut calShortcut = new Shortcut();
+		final MenuItem mailMenuItem = new MenuItem("Mail");
+		final MenuItem calMenuItem = new MenuItem("Calendar");
+		
+		SelectionListener<ComponentEvent> shortcutListener = new SelectionListener<ComponentEvent>() {
+			@Override
+			public void componentSelected(ComponentEvent ce) {
+				Window w = null;
+				if (ce.getSource() == mailShortcut) {
+					w = ce.getComponent().getData("mailWindow");
+				}
+				else if (ce.getSource() == calShortcut) {
+					w = ce.getComponent().getData("calendarWindow");
+				}
 				
-		Shortcut mailShortcut = new Shortcut();
+				if (!desktop.getWindows().contains(w)) {
+					desktop.addWindow(w);
+				}
+				
+				if (w != null && !w.isVisible()) {
+					w.show();
+				}
+				else {
+					w.toFront();
+				}
+			}
+		};
+		
+		SelectionListener<MenuEvent> menuListener = new SelectionListener<MenuEvent>() {
+			@Override
+			public void componentSelected(MenuEvent ce) {
+				Window w = null;
+				if (ce.getSource() == mailMenuItem) {
+					w = ce.getComponent().getData("mailWindow");
+				}
+				else if (ce.getSource() == calMenuItem) {
+					w = ce.getComponent().getData("calendarWindow");
+				}
+				
+				if (!desktop.getWindows().contains(w)) {
+					desktop.addWindow(w);
+				}
+				
+				if (w != null && !w.isVisible()) {
+					w.show();
+				}
+				else {
+					w.toFront();
+				}
+			}
+		};
+
+
 		mailShortcut.setText("Mail Client");
 		mailShortcut.setId("wappy-mail-shortcut");
-		mailShortcut.addSelectionListener(new SelectionListener<ComponentEvent>() {
-			public void componentSelected(ComponentEvent ce) {
-				openMail();
-			}
-		});
+		mailShortcut.setData("mailWindow", mailWindow);
+		mailShortcut.addSelectionListener(shortcutListener);
+		desktop.addShortcut(mailShortcut);
 		
-		Shortcut calShortcut = new Shortcut();
 		calShortcut.setText("Calendar");
 		calShortcut.setId("wappy-calendar-shortcut");
-		calShortcut.addSelectionListener(new SelectionListener<ComponentEvent>() {
-			public void componentSelected(ComponentEvent ce) {
-				openCalendar();
+		calShortcut.setData("calendarWindow", calendarWindow);
+		calShortcut.addSelectionListener(shortcutListener);
+		desktop.addShortcut(calShortcut);
+		
+		TaskBar taskBar = desktop.getTaskBar();
+
+		StartMenu startMenu = taskBar.getStartMenu();
+		startMenu.setHeading("Wappy developer");
+		startMenu.setIconStyle("wappy-startmenu-user");
+		
+		mailMenuItem.setData("window", mailWindow);
+		mailMenuItem.setTitle("Open the Mail Client");
+		mailMenuItem.setIconStyle("wappy-startmenu-mail");
+		mailMenuItem.addSelectionListener(menuListener);
+		startMenu.add(mailMenuItem);
+		
+		calMenuItem.setData("window", calendarWindow);
+		calMenuItem.setTitle("Open the Calendar");
+		calMenuItem.setIconStyle("wappy-startmenu-calendar");
+		calMenuItem.addSelectionListener(menuListener);
+		startMenu.add(calMenuItem);
+		
+		MenuItem logout = new MenuItem("Logout", new SelectionListener<MenuEvent>() {
+			@Override
+			public void componentSelected(MenuEvent ce) {
+				com.google.gwt.user.client.Window.Location.assign(
+			    "/accounts/logout/");
 			}
 		});
+		logout.setStyleName("wappy-startmenu-logout");
+		startMenu.addTool(logout);
 		
-		desktop.getDesktop().setStyleName("wappy-desktop-background");
-		desktop.addShortcut(mailShortcut);
-		desktop.addShortcut(calShortcut);
-				
-        desktop.getStartMenu().add(openMail);
-        desktop.getStartMenu().add(openCalendar);
-        desktop.getStartMenu().add(logout);
+
 	}
 	
 	private void openMail() {
